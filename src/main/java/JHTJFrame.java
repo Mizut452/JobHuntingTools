a375c9c9f2ae238db54fd8f3a4c10c80fe899d86
M1zut4 <ssvror444@gmail.com>
2022-12-01 00:53:14 +0900

    ボタンの設置

--- src/main/java/JHTJFrame.java
+++ src/main/java/JHTJFrame.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class JHTJFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class JHTJFrame extends JFrame {
+
+    JHTJFrame() {
+        Panel ComingSoon = new Panel();
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+        JButton companyResearchButton = new JButton("企業研究");
+        companyResearchButton.setBounds(250, 300, 100, 50);
+        panel.add(companyResearchButton);
+        getContentPane().add(panel, BorderLayout.CENTER);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("就活支援ツール");
+        setSize(600, 600);
+        setVisible(true);
+
+    }
+
 }
--- src/main/java/JobHuntingTools.java
+++ src/main/java/JobHuntingTools.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class JobHuntingTools {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class JobHuntingTools extends JFrame {
+    public static JHTJFrame jHTFframe;
+
+
+    public static void main(String[] args) {
+            jHTFframe = new JHTJFrame();
+    }
 }
