4f77753f0b92c2c1de3985e156650f3330af76c7
M1zut4 <ssvror444@gmail.com>
2022-12-01 22:43:50 +0900

    テキストエリア作成

--- src/main/java/ResearchPanel.java
+++ src/main/java/ResearchPanel.java
@@ -4,6 +4,14 @@ public class ResearchPanel extends Panel {
 
     ResearchPanel() {
         setLayout(null);
+        Panel textPanel = new Panel();
+        TextField companyName = new TextField();
+        companyName.setBounds(100, 100, 100, 100);
+        add(companyName);
+
+
+
+
     }
 
 
