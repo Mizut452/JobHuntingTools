099beb96a5ede17688bedd715938becde32eb121
M1zut4 <ssvror444@gmail.com>
2022-12-01 17:19:12 +0900

    ボタンの設置とタイトルを出す

--- src/main/java/TitlePanel.java
+++ src/main/java/TitlePanel.java
@@ -4,5 +4,18 @@ public class TitlePanel extends Panel {
 
     TitlePanel() {
         setLayout(null);
+        Button startResearch = new Button("企業研究");
+        startResearch.setBounds(250, 100, 200, 200);
+        startResearch.addActionListener(e -> {
+            Frame frame = (Frame) getParent();
+            CardLayout cardLayout = (CardLayout) frame.getLayout();
+            cardLayout.next(frame);
+        });
+        add(startResearch);
+
+        Label titleCall = new Label("就活支援ツール");
+        titleCall.setBounds(200, 200, 200, 200);
+        titleCall.setFont(new Font("serif", Font.PLAIN, 30));
+        add(titleCall);
     }
 }
