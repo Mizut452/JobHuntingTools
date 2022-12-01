e2180f3ef0a338437048089eb9f14099c56373c6
M1zut4 <ssvror444@gmail.com>
2022-12-01 17:23:06 +0900

    タイトル画面完成

--- src/main/java/TitlePanel.java
+++ src/main/java/TitlePanel.java
@@ -5,7 +5,7 @@ public class TitlePanel extends Panel {
     TitlePanel() {
         setLayout(null);
         Button startResearch = new Button("企業研究");
-        startResearch.setBounds(250, 100, 200, 200);
+        startResearch.setBounds(250, 300, 120, 50);
         startResearch.addActionListener(e -> {
             Frame frame = (Frame) getParent();
             CardLayout cardLayout = (CardLayout) frame.getLayout();
@@ -14,8 +14,8 @@ public class TitlePanel extends Panel {
         add(startResearch);
 
         Label titleCall = new Label("就活支援ツール");
-        titleCall.setBounds(200, 200, 200, 200);
-        titleCall.setFont(new Font("serif", Font.PLAIN, 30));
+        titleCall.setBounds(70, -200, 450, 600);
+        titleCall.setFont(new Font("serif", Font.PLAIN, 60));
         add(titleCall);
     }
 }
